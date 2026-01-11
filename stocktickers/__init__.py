149b6c9f6567f98f308fd2d5cf0fac0a10911a67
tahashieenavaz <tahashieenavaz@gmail.com>
2026-01-11 17:46:31 +0100

    adds UK tickers

--- stocktickers/__init__.py
+++ stocktickers/__init__.py
@@ -2,3 +2,5 @@ from .japan import tickers as japan
 from .japan import tickers as tokyo
 from .germany import tickers as germany
 from .germany import tickers as frankfurt
+from .uk import tickers as uk
+from .uk import tickers as london
